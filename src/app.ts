586aeb3ff7bbb52b68ead8e87e4959b06f1cdd1d
devmilon923 <dev.milon923@gmail.com>
2026-07-06 12:39:05 +0600

    feat: add cookie-parser middleware and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import dotenv from "dotenv";
 import router from "./routes";
 import globalErrorHandler from "./middleware/globalError";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [process.env.CLIENTDOMAIN || "*"],
     credentials: true,
   }),
 );
